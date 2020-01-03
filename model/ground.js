51b984d43865a2bbf6cad8bdacc3560e5fddd619
super <supereggsong@gmail.com>
2020-01-03 15:30:11 +0900

    경기장 등록

--- model/ground.js
+++ model/ground.js
@@ -5,14 +5,18 @@ const groundSchema = new Schema({
   id: mongoose.Schema.Types.ObjectId,
   groundName: {type: String, required: true},   // 경기장명
   groundAddress: {jibun: String, road: String},   // 경기장 주소
-  image_url: [String],                          // 경기장 이미지
+  ground_images: {
+    ground_images_1:String,
+    ground_images_2:String,
+    ground_images_3:String
+  },                          // 경기장 이미지
   mapInfo: {Lat: Number, Lng: Number},          // 경기장 위치정보
   facility: {
     size: {x: Number, y: Number},
-    shower: Boolean,
-    freeParking: Boolean,
-    shoesRental: Boolean,
-    uniformRental: Boolean
+    shower: {type:Boolean, default:false},
+    freeParking: {type:Boolean, default:false},
+    shoesRental: {type:Boolean, default:false},
+    uniformRental: {type:Boolean, default:false}
   },                                        // 경기장 시설
   description: String,                     // 경기장 특이사항
   updated: {type: Date, default: Date.now}
--- routes/admin_ground.js
+++ routes/admin_ground.js
@@ -2,23 +2,71 @@ const express = require('express');
 const router = express.Router();
 const Ground = require('../model/ground');
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   let user = req.session.passport.user;
   let list = await Ground.aggregate([
-    {$match:{}}
+    {$match: {}}
   ]);
-  res.render('admin_ground',{
-    active:'ground',
-    user:user,
-    list:list,
-    title:'경기장 관리'
+  res.render('admin_ground', {
+    active: 'ground',
+    user: user,
+    list: list,
+    title: '경기장 관리'
   });
 });
-router.get('/register', async(req, res) => {
+router.get('/register/:id?', async (req, res) => {
+  let ground = null;
+  if(req.params.id) ground = await Ground.findOne({_id:req.params.id});
   res.render('admin_ground_register', {
-    active:'ground',
-    title:'경기장 등록'
+    active: 'ground',
+    title: '경기장 등록',
+    ground:ground
   });
+});
+// router.get('/register/:id', async(req, res) => {
+//   res.render('admin_ground_register', {
+//     active:'ground',
+//     title:'경기장 관리',
+//     ground:ground
+//   })
+// });
+router.post('/register', async (req, res) => {
+  try{
+    let insertDoc = {
+      groundName: req.body.groundName,
+      groundAddress: {
+        jibun:req.body.jibun,
+        road:req.body.road
+      },
+      ground_images: {
+        ground_images_1:req.body.ground_images.ground_images_1 || '',
+        ground_images_2:req.body.ground_images.ground_images_2 || '',
+        ground_images_3:req.body.ground_images.ground_images_3 || ''
+      },
+      mapInfo: {Lat: Number(req.body.y), Lng: Number(req.body.x)},
+      facility: {
+        size: {x: Number(req.body.groundSizeX), y: Number(req.body.groundSizeY)},
+        shower: req.body.facility.indexOf('shower') > -1,
+        freeParking: req.body.facility.indexOf('freeParking') > -1,
+        shoesRental: req.body.facility.indexOf('shoesRental') > -1,
+        uniformRental: req.body.facility.indexOf('uniformRental') > -1
+      },
+      description: req.body.description
+    };
+    // INSERT OR UPDATE
+    let isUpdate = req.body.isUpdate, result;
+    if(!isUpdate) result = await Ground.create(insertDoc);
+    else result = await Ground.updateOne({_id:req.body.ground_id}, {$set:insertDoc});
+
+    if(result){
+      res.json({code:1, message:'등록성공', result:result});
+    }else{
+      res.json({code:0, message:'등록실패'});
+    }
+  }catch(e){
+    console.error(e);
+    res.json({code:0, message:'등록실패!'})
+  }
 });
 
 module.exports = router;

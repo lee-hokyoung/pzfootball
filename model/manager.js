2486d423343c235ea153aa3ff9c8481a493dbb7d
lee-hokyoung <supereggsong@gmail.com>
2020-03-04 19:12:29 +0900

    pc commit

--- model/manager.js
+++ model/manager.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ManagerSchema = new Schema({
+  pz_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   manager_id: { type: String, required: true, unique: true },
   manager_pw: String,
   manager_name: String,
@@ -9,12 +10,7 @@ const ManagerSchema = new Schema({
   user_phone: String,
   user_addr: String,
   activity_area: String,
-  user_nickname: String,
-  snsId: String,
-  provider: String,
   profile_image: String,
-  thumbnail_image: String,
-  point: Number,
   created_at: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model("Manager", ManagerSchema);
--- routes/admin_manager.js
+++ routes/admin_manager.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const Manager = require("../model/manager");
 
 // 클럽 리스트 가져오기
 router.get("/list", async (req, res) => {
@@ -11,5 +12,13 @@ router.get("/list", async (req, res) => {
     title: "퍼즐풋볼 - 매니저 관리",
     manager_list: []
   });
+});
+router.post("/register", async (req, res) => {
+  try {
+    let result = await Manager.create(req.body);
+    res.json({ code: 1, message: "등록 성공", result: result });
+  } catch (err) {
+    res.json({ code: 0, message: err.message });
+  }
 });
 module.exports = router;

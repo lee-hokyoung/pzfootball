d675946f8ea14b3776d7f27fe2c8a09a825b3bac
lee-hokyoung <supereggsong@gmail.com>
2020-04-18 19:40:52 +0900

    관리자 페이지 회원 정보 경기 매치수

--- routes/admin.js
+++ routes/admin.js
@@ -51,121 +51,139 @@ router.post("/login", middle.isNotLoggedIn, (req, res, next) => {
 
 //  회원관리
 router.get("/user/list", middle.isAdmin, async (req, res) => {
-  let list = await User.aggregate([
-    { $match: { admin: false } },
-    { $sort: { created_at: 1 } },
-    {
-      $project: {
-        user_id: 1,
-        user_name: 1,
-        user_phone: 1,
-        phone1: 1,
-        phone2: 1,
-        point: 1,
-        point_history: 1,
-        manner: 1,
-        created_at: 1,
+  try {
+    let ladder = req.query.ladder;
+    let ladder_query = [1, 1];
+    if (ladder) ladder_query = ["$ladder", parseInt(ladder)];
+    console.log("ladder : ", ladder_query);
+    let list = await User.aggregate([
+      { $match: { admin: false } },
+      { $sort: { created_at: 1 } },
+      {
+        $project: {
+          user_id: 1,
+          user_name: 1,
+          user_phone: 1,
+          phone1: 1,
+          phone2: 1,
+          point: 1,
+          point_history: 1,
+          manner: 1,
+          created_at: 1,
+        },
       },
-    },
-    {
-      $lookup: {
-        from: "club",
-        let: { local_id: "$_id" },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                $gt: [
-                  { $indexOfArray: ["$club_member._id", "$$local_id"] },
-                  -1,
-                ],
+      {
+        $lookup: {
+          from: "club",
+          let: { local_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $gt: [
+                    { $indexOfArray: ["$club_member._id", "$$local_id"] },
+                    -1,
+                  ],
+                },
               },
             },
-          },
-          { $project: { club_mark: 0 } },
-        ],
-        as: "club_info",
+            { $project: { club_mark: 0 } },
+          ],
+          as: "club_info",
+        },
       },
-    },
-    { $unwind: { path: "$club_info", preserveNullAndEmptyArrays: true } },
-    {
-      $lookup: {
-        from: "club",
-        let: { local_id: "$_id" },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                $gt: [{ $indexOfArray: ["$waiting_member", "$$local_id"] }, -1],
+      { $unwind: { path: "$club_info", preserveNullAndEmptyArrays: true } },
+      {
+        $lookup: {
+          from: "club",
+          let: { local_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $gt: [
+                    { $indexOfArray: ["$waiting_member", "$$local_id"] },
+                    -1,
+                  ],
+                },
               },
             },
-          },
-          { $project: { _id: 1 } },
-        ],
-        as: "waiting_info",
+            { $project: { _id: 1 } },
+          ],
+          as: "waiting_info",
+        },
       },
-    },
-    {
-      $unwind: {
-        path: "$waiting_info",
-        preserveNullAndEmptyArrays: true,
+      {
+        $unwind: {
+          path: "$waiting_info",
+          preserveNullAndEmptyArrays: true,
+        },
       },
-    },
-    {
-      $lookup: {
-        from: "match",
-        let: { local_id: "$_id" },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                $gt: [
-                  { $indexOfArray: ["$apply_member._id", "$$local_id"] },
-                  -1,
-                ],
+      {
+        $lookup: {
+          from: "match",
+          let: { local_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    {
+                      $gt: [
+                        { $indexOfArray: ["$apply_member._id", "$$local_id"] },
+                        -1,
+                      ],
+                    },
+                    {
+                      $eq: ladder_query,
+                    },
+                  ],
+                },
               },
             },
-          },
-          { $project: { apply_member: 1, mvp: 1 } },
-        ],
-        as: "match_info",
+            { $project: { apply_member: 1, mvp: 1 } },
+          ],
+          as: "match_info",
+        },
       },
-    },
-    { $unwind: { path: "$match_info", preserveNullAndEmptyArrays: true } },
-    {
-      $lookup: {
-        from: "manners",
-        localField: "match_info.apply_member.penalty",
-        foreignField: "_id",
-        as: "manner_info",
+      { $unwind: { path: "$match_info", preserveNullAndEmptyArrays: true } },
+      {
+        $lookup: {
+          from: "manners",
+          localField: "match_info.apply_member.penalty",
+          foreignField: "_id",
+          as: "manner_info",
+        },
       },
-    },
-    {
-      $group: {
-        _id: "$_id",
-        user_id: { $first: "$user_id" },
-        user_name: { $first: "$user_name" },
-        user_phone: { $first: "$user_phone" },
-        phone1: { $first: "$phone1" },
-        phone2: { $first: "$phone2" },
-        point: { $first: "$point" },
-        point_history: { $first: "$point_history" },
-        club_info: { $first: "$club_info" },
-        waiting_info: { $first: "$waiting_info" },
-        created_at: { $first: "$created_at" },
-        match_count: { $sum: 1 },
-        match_data: { $push: "$match_info" },
-        manner_info: { $push: "$manner_info" },
+      {
+        $group: {
+          _id: "$_id",
+          user_id: { $first: "$user_id" },
+          user_name: { $first: "$user_name" },
+          user_phone: { $first: "$user_phone" },
+          phone1: { $first: "$phone1" },
+          phone2: { $first: "$phone2" },
+          point: { $first: "$point" },
+          point_history: { $first: "$point_history" },
+          club_info: { $first: "$club_info" },
+          waiting_info: { $first: "$waiting_info" },
+          created_at: { $first: "$created_at" },
+          match_data: { $push: "$match_info" },
+          manner_info: { $push: "$manner_info" },
+        },
       },
-    },
-  ]);
-  let user = req.session.passport.user;
-  res.render("admin_user", {
-    active: "user",
-    title: "퍼즐풋볼 - 회원관리",
-    list: list,
-    user: user,
-  });
+    ]);
+    let user = req.session.passport.user;
+    res.render("admin_user", {
+      active: "user",
+      title: "퍼즐풋볼 - 회원관리",
+      list: list,
+      user: user,
+      ladder: req.query.ladder,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 //  회원관리 - 삭제
 router.delete("/user/delete/:id", middle.isAdmin, async (req, res) => {
